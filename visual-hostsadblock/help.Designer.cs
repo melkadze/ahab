501bce5e9151e35935f58669fc6b978d489c7427
melkadze <25963683+melkadze@users.noreply.github.com>
2019-06-10 01:07:08 -0400

    reupload

--- visual-hostsadblock/help.Designer.cs
+++ visual-hostsadblock/help.Designer.cs
@@ -110,7 +110,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 91);
+            this.ClientSize = new System.Drawing.Size(304, 87);
             this.ControlBox = false;
             this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
